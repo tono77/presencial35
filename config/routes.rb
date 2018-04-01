bff74f90d6725c9bea91aac1b558369294f23b3b
tono77 <edgardo.ochoa@gmail.com>
2018-04-01 13:09:48 -0300

    presencial35 yapoApp

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  resources :products
+  devise_for :users, controllers: {
+        registrations: 'users/registrations'
+      }
+  root "products#index"
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+end
